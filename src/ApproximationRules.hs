b0855786b66cc0f20cf5bf3b9c53dcbd404849da
FlyingCheburek <thomashenriquesantospinheiro@gmail.com>
2025-07-05 17:42:52 -0300

    chore: edit libraries (Cabal dependecies)

---
+++ src/ApproximationRules.hs
@@ -0,0 +1,14 @@
+module ApproximationRules (
+    leftsum,
+    rightsum,
+    averagesum
+) where
+
+leftsum :: Double -> Double
+leftsum x = x
+
+rightsum :: Double -> Double
+rightsum x = x
+
+averagesum :: Double -> Double
+averagesum x = x 
